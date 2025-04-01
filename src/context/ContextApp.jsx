dde313f43ddf719545af8ab56ea3e39575f4ae28
Abdelrahman <abdoesmailahmedkhalaph2492002@gmail.com>
2025-04-01 15:04:47 +0200

    Use useReducer hook instead of useState , add useCallBack to functions ,and using useMemo

--- src/context/ContextApp.jsx
+++ src/context/ContextApp.jsx
@@ -1,26 +1,68 @@
-import { createContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useEffect,
+  useMemo,
+  useReducer,
+} from "react";
 import { dummyCourses } from "../assets/assets";
 import humanizeDuration from "humanize-duration";
 
 export const MyContext = createContext();
 
+const initalState = {
+  coursesData: [],
+  enrollmentCourses: [],
+  coursesForView: [],
+  playerData: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_COURSES_DATA": {
+      return {
+        ...state,
+        coursesData: action.payload,
+      };
+    }
+    case "SET_ENROLLMENT_COURSES": {
+      return {
+        ...state,
+        enrollmentCourses: action.payload,
+      };
+    }
+    case "SET_COURSES_FOR_VIEW": {
+      return {
+        ...state,
+        coursesForView: action.payload.slice(0, 4),
+      };
+    }
+    case "SET_PLAYER_DATA": {
+      return {
+        ...state,
+        playerData: action.payload,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
 const AppProvider = ({ children }) => {
-  const [coursesData, setCoursesData] = useState([]);
-  const [enrollmentCourses, setEnrollmentCourses] = useState([]);
-  const [coursesForView, setCoursesForView] = useState([]);
-  const [playerData, setPlayerData] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initalState);
 
   const currency = import.meta.env.VITE_CURRENCY;
 
-  const fetchCoursesData = async () => {
-    setCoursesData(dummyCourses);
-  };
+  const fetchCoursesData = useCallback(async () => {
+    dispatch({ type: "SET_COURSES_DATA", payload: dummyCourses });
+  }, []);
 
-  const fetchEnrollmentCourses = async () => {
-    setEnrollmentCourses(dummyCourses);
-  };
+  const fetchEnrollmentCourses = useCallback(async () => {
+    dispatch({ type: "SET_ENROLLMENT_COURSES", payload: dummyCourses });
+  }, []);
 
-  const shortTimeFormat = (time) => {
+  const shortTimeFormat = useCallback((time) => {
     return humanizeDuration(time * 60 * 1000, {
       language: "shortEn",
       spacer: "",
@@ -32,15 +74,15 @@ const AppProvider = ({ children }) => {
         },
       },
     });
-  };
+  }, []);
 
-  const timeFormat = (time) => {
+  const timeFormat = useCallback((time) => {
     return humanizeDuration(time * 60 * 1000, {
       units: ["h", "m"],
     });
-  };
+  }, []);
 
-  const calculateRating = (rating) => {
+  const calculateRating = useCallback((rating) => {
     if (!rating.length) return 0;
     const totalRating = rating.reduce(
       (total, current) => total + current.rating,
@@ -48,37 +90,43 @@ const AppProvider = ({ children }) => {
     );
 
     return totalRating / rating.length;
-  };
+  }, []);
 
-  const calculateDiscount = (price, discount) => {
+  const calculateDiscount = useCallback((price, discount) => {
     return (price - (price * discount) / 100).toFixed(2);
-  };
+  }, []);
 
-  const calculateCourseDurations = (course) => {
-    if (course.courseContent.length === 0) return 0;
-    const totalTime = course.courseContent.reduce((total, chapter) => {
-      return (
-        total +
-        chapter.chapterContent.reduce(
-          (sum, lecture) => sum + lecture.lectureDuration,
-          0,
-        )
+  const calculateCourseDurations = useCallback(
+    (course) => {
+      if (course.courseContent.length === 0) return 0;
+      const totalTime = course.courseContent.reduce((total, chapter) => {
+        return (
+          total +
+          chapter.chapterContent.reduce(
+            (sum, lecture) => sum + lecture.lectureDuration,
+            0,
+          )
+        );
+      }, 0);
+      return timeFormat(totalTime);
+    },
+    [timeFormat],
+  );
+
+  const calculateChapterDuration = useCallback(
+    (chapter) => {
+      if (!chapter.chapterContent.length) return 0;
+      const chapterDuration = chapter.chapterContent.reduce(
+        (total, lecture) => total + lecture.lectureDuration,
+        0,
       );
-    }, 0);
-    return timeFormat(totalTime);
-  };
 
-  const calculateChapterDuration = (chapter) => {
-    if (!chapter.chapterContent.length) return 0;
-    const chapterDuration = chapter.chapterContent.reduce(
-      (total, lecture) => total + lecture.lectureDuration,
-      0,
-    );
+      return shortTimeFormat(chapterDuration);
+    },
+    [shortTimeFormat],
+  );
 
-    return shortTimeFormat(chapterDuration);
-  };
-
-  const calculateLecturesCount = (course) => {
+  const calculateLecturesCount = useCallback((course) => {
     let totalCount = 0;
     course.courseContent.forEach((chapter) => {
       if (Array.isArray(chapter.chapterContent)) {
@@ -86,28 +134,48 @@ const AppProvider = ({ children }) => {
       }
     });
     return totalCount;
-  };
+  }, []);
 
-  const getVideoId = (lectureUrl, index, i) => {
-    setPlayerData({
-      videoId: lectureUrl.split("/").pop(),
-      chapter: index + 1,
-      lecture: i + 1,
+  const getVideoId = useCallback((lectureUrl, index, i) => {
+    dispatch({
+      type: "SET_PLAYER_DATA",
+      payload: {
+        videoId: lectureUrl.split("/").pop(),
+        chapter: index + 1,
+        lecture: i + 1,
+      },
     });
-  };
+  }, []);
 
   useEffect(() => {
     fetchCoursesData();
     fetchEnrollmentCourses();
-    if (coursesData.length) {
-      setCoursesForView(coursesData.slice(0, 4));
+    if (state.coursesData.length) {
+      dispatch({ type: "SET_COURSES_FOR_VIEW", payload: state.coursesData });
     }
-  }, [coursesData, enrollmentCourses]);
+  }, [state.coursesData, fetchEnrollmentCourses, fetchCoursesData]);
 
-  const value = {
-    coursesData,
-    coursesForView,
-    enrollmentCourses,
+  const value = useMemo(() => {
+    return {
+      coursesData: state.coursesData,
+      coursesForView: state.coursesForView,
+      enrollmentCourses: state.enrollmentCourses,
+      playerData: state.playerData,
+      calculateRating,
+      currency,
+      calculateDiscount,
+      calculateCourseDurations,
+      calculateChapterDuration,
+      calculateLecturesCount,
+      shortTimeFormat,
+      timeFormat,
+      getVideoId,
+    };
+  }, [
+    state.coursesData,
+    state.coursesForView,
+    state.enrollmentCourses,
+    state.playerData,
     calculateRating,
     currency,
     calculateDiscount,
@@ -116,9 +184,8 @@ const AppProvider = ({ children }) => {
     calculateLecturesCount,
     shortTimeFormat,
     timeFormat,
-    playerData,
     getVideoId,
-  };
+  ]);
   return <MyContext.Provider value={value}>{children}</MyContext.Provider>;
 };
 
